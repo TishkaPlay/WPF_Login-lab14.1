919d7148c9a035156666644e3ea9b716f0772523
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-05 19:08:29 +0300

    Add project files.

---
+++ LoginView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WPF_Login
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.Content = new InvoiceView();
+        }
+    } 
+}
